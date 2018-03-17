c61d49bed33b876dd333fed3a7c583b3a88bcd4c
Michael Adams <mtadams007@gmail.com>
2018-03-17 14:47:38 -0400

    added buddies, hobbits and ring

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -27,12 +27,18 @@ middleEarth.setAttribute("id", "middle-earth")
 
 
 let makeMiddleEarth = () => {
+  //add middle earth to the body
   body.appendChild(middleEarth);
   let i=0;
+  //cycle through lands
   while (i<lands.length) {
+    //create each article
     let land = document.createElement('article')
+    //append each article
     middleEarth.appendChild(land);
+    //give each land their class name
     land.className = (lands[i]);
+    //create the headers
     let header = document.createElement('h1');
     header.innerText = lands[i];
     land.appendChild(header);
@@ -42,21 +48,67 @@ let makeMiddleEarth = () => {
 
 makeMiddleEarth();
 
-const hobbiton = document.getElementsByClassName('The Shire');
+let hobbiton = document.getElementsByClassName('The Shire');
 
 let makeHobbits = () => {
   let i=0;
-  // let home = document.getElementsByClassName('The Shire');
-
+//create unordered list
   let halflings = document.createElement('ul');
-  hobbiton.appendChild(halflings);
+  //hobbiton is an array, so have to append to first and only element
+  hobbiton[0].appendChild(halflings);
   while (i<hobbits.length) {
     let hobbit = document.createElement('li')
     halflings.appendChild(hobbit);
-    hobbit.innerText(hobbits[i]);
+    //to add text to a li
+    hobbit.append(document.createTextNode(hobbits[i]));
+    hobbit.className = ('hobbit');
     i++
+  }
+}
+
+const halfling = document.getElementsByClassName('hobbit');
+
+let keepItSecretKeepItSafe = () => {
+  let ring = document.createElement('div');
+  ring.className = 'magic-imbued-jewelry';
+  ring.setAttribute('id', 'the-ring');
+  halfling[0].appendChild(ring);
+}
+
+const rivendell = document.getElementsByClassName('Rivendell');
+
+let makeBuddies = () => {
+  let aside = document.createElement('aside');
+  let fellowship = document.createElement('ul');
+  rivendell[0].appendChild(aside);
+  aside.appendChild(fellowship);
+  let i=0;
+  while (i<buddies.length) {
+    let fellow = document.createElement('li');
+    aside.appendChild(fellow);
+    fellow.innerHTML = buddies[i];
+    i++;
   }
 }
 
 
 makeHobbits();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+keepItSecretKeepItSafe();
+
+makeBuddies();
